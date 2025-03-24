7f3c532825dd53b511c7337d1baedb7afd470642
khoiru Attar <160084153+Attaroke57@users.noreply.github.com>
2025-03-25 01:22:43 +0700

    Revert "Feat/schedulepage"

--- lib/Page/RegisterPage/binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:user_ocean_learn/Page/RegisterPage/controller.dart';
-
-class RegisterBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<RegisterController>(() => RegisterController());
-  }
-}
